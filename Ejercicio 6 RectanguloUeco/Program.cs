0a1eae635fdd5b80e16e17a71c4787ceb23a91bd
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:30:24 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio 6 RectanguloUeco/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejercicio_6_RectanguloUeco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo(11, 20,"f");
+        }
+        public static void Rectangulo(int altura, int anchura,string letra)
+        {
+            for (int i = 0; i <= (altura - 1); i++)
+            {
+                for (int f = 0; f <= (anchura-1 ); f++)
+                {
+                    if ((i==0)||(i==altura-1)) {
+                        Console.Write(letra+letra+letra);
+                        Console.Write(" ");
+                    }
+                    else if ((f == 0) || (f == anchura-1))
+                    {
+                        Console.Write(letra + letra + letra);
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                        Console.Write(" ");
+                    }
+                    
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
